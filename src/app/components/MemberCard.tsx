027d5e5fff51e5be23d54b00544a3527d282000d
babyinthesky <leishuang119@gmail.com>
2023-08-29 23:20:28 +0300

    added empty list and card componenet

---
+++ src/app/components/MemberCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MemberCard = (id: string) => {
+
+  // TODO: fetch detailed data
+  return (
+    <div className="">
+    </div>
+  )
+}
+
+export default MemberCard;
---
+++ src/app/components/MemberGridList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MemberGridList = () => {
+
+  // TODO: load id list and location
+  // members
+  return (
+    <div className="">
+      {/* members.map(() => (
+        <MemberCard />
+      )) */}
+    </div>
+  )
+}
+
+export default MemberGridList;
