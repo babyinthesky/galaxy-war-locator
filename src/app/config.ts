1fa79afba19d6e7c2fdda705107ae44171a927dd
babyinthesky <leishuang119@gmail.com>
2023-08-29 22:18:17 +0300

    added config file for storing url

---
+++ src/app/config.ts
@@ -0,0 +1,4 @@
+export const SECRET_URL = 'https://aseevia.github.io/star-wars-frontend/data/secret.json';
+
+export const getDataPrefixUrl = (id: string) => `https://akabab.github.io/starwars-api/api/${id}.json`;
+
