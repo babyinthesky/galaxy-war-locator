caa96661029eafef7b3173d4b47f57594ab9b078
babyinthesky <leishuang119@gmail.com>
2023-08-31 02:01:23 +0300

    fix import error

--- src/app/redux/hooks.ts
+++ src/app/redux/hooks.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch, AppThunkDispatch } from './store';
+import type { RootState, AppDispatch, AppThunkDispatch } from './types';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppTrunkDispatch: () => AppThunkDispatch = useDispatch;
