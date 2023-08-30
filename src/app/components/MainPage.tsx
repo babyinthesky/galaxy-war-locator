8d89e963a72eb5b8b4405f30d0dca83176f58600
babyinthesky <leishuang119@gmail.com>
2023-08-30 09:36:56 +0300

    clean up main page

--- src/app/components/MainPage.tsx
+++ src/app/components/MainPage.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import React, { useState, useEffect, FC } from 'react';
+import React, { useEffect } from 'react';
 import { useAppTrunkDispatch, useAppSelector } from '../redux/hooks';
-import { SECRET_URL } from '../config';
 import LoadingSpinner from './LoadingSpinner';
 import dynamic from 'next/dynamic';
 import { selectMemberShortList, getMemberShortList } from '../redux/memberShortListSlice';
@@ -11,30 +10,11 @@ import { selectMemberShortList, getMemberShortList } from '../redux/memberShortL
 const LocaterMap = dynamic(() => import('./LocatorMap'), { ssr: false })
 
 const MainPage = () => {
-  const [errorText, setErrorText] = useState('');
-  // const [isLoading, setIsLoading] = useState(false);
-
   const dispatch = useAppTrunkDispatch();
   const { list: memberShortList, isLoading, hasError } = useAppSelector(selectMemberShortList);
-  // const {loading, error, member} = usersList
 
   useEffect(() => {
-      // setIsLoading(true);
       dispatch(getMemberShortList())
-    //   fetch(SECRET_URL).then((res) => {
-    //   setIsLoading(false);
-    //   if (res.ok) {
-    //     setErrorText('');
-    //     return res.json();
-    //   }
-    //   throw new Error(`HTTP ${res.status}`);
-    // }).then((data) => {
-    //   const message = atob(data.message);
-    //   console.log(message); // TODO: store to redux
-    // }).catch((error) => {
-    //   setIsLoading(false);
-    //   setErrorText(error.message);
-    // });
   }, [dispatch]);
 
   if (isLoading) {
@@ -43,16 +23,14 @@ const MainPage = () => {
       );
   }
 
-  console.log({memberShortList})
   return (
-          <div className="flex flex-col flex-1 flex-wrap min-h-[80%] w-full content-center">
-        <h1 className="text-center">Star Wars - Rebellion: Locator</h1>
-        <p>{errorText}</p>
-        <LocaterMap />
-        {/* <MemberGridList />*/}
+    <div className="flex flex-col flex-1 flex-wrap min-h-[80%] w-full content-center">
+      <h1 className="text-center">Star Wars - Rebellion: Locator</h1>
+      <p>{hasError && 'Loading Error'}</p>
+      <LocaterMap />
+      {/* <MemberGridList />*/}
     </div>
   );
 };
 
 export default MainPage;
-// export default wrapper.withRedux(MainPage);
