e2e0488969b7f9c34c815a4442d47b1b5000cd39
babyinthesky <leishuang119@gmail.com>
2023-09-01 01:46:47 +0300

    fix jest setup

--- src/app/redux/types.ts
+++ src/app/redux/types.ts
@@ -1,8 +1,9 @@
 import { ThunkDispatch, AnyAction } from "@reduxjs/toolkit"
 import { store } from "./store";
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppStore = typeof store;
+export type AppDispatch = typeof store.dispatch;
 export type AppThunkDispatch = ThunkDispatch<RootState, any, AnyAction>;
 
 export type MemberShortInfo = {
